3238198993062e2f37ea58edb3a9e7298089c5c0
Daniel Ezekiel <danielihazkel@gmail.com>
2023-12-27 20:39:12 +0200

    .

--- src/main/java/com/bezkoder/spring/thymeleaf/pagingsorting/controller/TutorialController.java
+++ src/main/java/com/bezkoder/spring/thymeleaf/pagingsorting/controller/TutorialController.java
@@ -30,7 +30,7 @@ public class TutorialController {
   @GetMapping("/tutorials")
   public String getAll(Model model, @RequestParam(required = false) String keyword,
       @RequestParam(defaultValue = "1") int page,
-      @RequestParam(defaultValue = "6") int size,
+      @RequestParam(defaultValue = "10") int size,
       @RequestParam(defaultValue = "id,asc") String[] sort) {
     try {
       List<Tutorial> tutorials = new ArrayList<Tutorial>();
